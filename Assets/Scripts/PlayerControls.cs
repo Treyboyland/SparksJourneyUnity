0b527cefe524ec068d88ca29155a1a00d981eba6
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-08 12:14:50 -0500

    Create PlayerControls.cs
    General event controls

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameEventGeneric<Vector2> onMove;
+
+[SerializeField]
+GameEvent onAbility;
+
+[SerializeField]
+GameEvent onJump;
+
+[SerializeField]
+GameEvent onQuit;
+
+[SerializeField]
+GameEvent onScreenshot;
+
+[SerializeField]
+GameEvent onPause;
+
+[SerializeField]
+GameEvent onReset;
+
+[SerializeField]
+GameEvent onSwap;
+
+public void OnMove(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onMove.Invoke(context.ReadValue<Vector2>());
+}
+}
+}
+
+public void OnAbility(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onAbility.Invoke();
+}
+}
+}
+
+public void OnJump(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onJump.Invoke();
+}
+}
+}
+
+public void OnQuit(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onQuit.Invoke();
+}
+}
+}
+
+public void OnReset(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onReset.Invoke();
+}
+}
+}
+
+public void OnScreenshot(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onScreenshot.Invoke();
+}
+}
+}
+
+public void OnSwap(InputAction.CallbackContext context)
+{
+if(context.ReadValueAsButton())
+{
+onSwap.Invoke();
+}
+}
+
+
+}
