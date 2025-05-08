22bfd6372fe80fd2f8b14d499e1b250643ab0eb1
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-08 12:25:53 -0500

    Create LevelButton.cs
    Loads scene on click

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TextMeshPro;
+
+public class LevelButton : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+
+[SerializeField]
+TMP_Text textBox;
+
+public Scene SceneToLoad {get; set;}
+
+public int Index 
+{
+set
+{
+textBox.text = "" + value;
+}
+}
+
+void Start()
+{
+button.onClick.AddListener(LoadScene);
+}
+
+void LoadScene()
+{
+SceneManager.LoadScene(SceneToLoad);
+}
+}
