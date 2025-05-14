c23d0b0d71ac35e178f3f821f93c7d426f949572
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-14 12:34:44 -0500

    Create PlayerSpark.cs

---
+++ Assets/Scripts/PlayerSpark.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerSpark : MonoBehaviour 
+{
+    [SerializeField]
+    float maxMovementDistance;
+
+[SerializeField]
+float speed;
+
+[SerializeField]
+RigidBody2D body;
+
+[SerializeField]
+GameEventGeneric <Vector3> onActivated;
+
+Vector3 anchorPosition;
+
+public void Activate(Vector3 startPos)
+{
+transform.position = startPos; 
+anchorPosition = startPos;
+onActivated.Invoke(startPos);
+gameObject.SetActive(true);
+}
+
+void FixedUpdate ()
+{
+if(Vector3.Distance(anchorPosition, transform.position) > maxMovementDistance)
+{
+Vector3 norm = (transform.position- anchorPosition).normalized ;
+body.MovePosition(anchorPosition+norm);
+}
+}
+
+}
