d053c87f91243d493931af7887ab2be79a554e04
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-14 09:50:12 -0500

    Create ObjectPool.cs

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool<T> : MonoBehaviour where T: MonoBehaviour 
+{
+    [SerializeField]
+    protected T prefab;
+
+protected List<T> pool = new List<T>();
+
+    public virtual T GetObject()
+{
+foreach(var obj in pool)
+{
+if(!obj.gameObject.activeInHierarchy)
+{
+return obj;
+}
+}
+
+return CreateObject();
+}
+
+protected T CreateObject()
+{
+var newObj = Instantiate(prefab) as T
+newObj.gameObject.SetActive(false);
+pool.Add(newObj);
+return newObj;
+}
+}
