7adf1ffe394667a9e86470c63ba4ac08e6791fd4
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-14 12:16:35 -0500

    Create GameEventDataLogPickupData.cs

---
+++ Assets/Scripts/Events/GameEventDataLogPickupData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="GameEvent-DataLogPickup-On",menuName="Events/Data Log Pickup"]
+public class GameEventDataLogPickupData : GameEventGeneric<DataLogPickupData>
+{
+}
