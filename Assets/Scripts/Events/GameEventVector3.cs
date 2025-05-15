a02b5179c49fcab3a1cf4a68861694ed038c799e
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-15 12:25:23 -0500

    Create GameEventVector3.cs

---
+++ Assets/Scripts/Events/GameEventVector3.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="GameEvent-Vector3-On",menuName="Events/Vector3"]
+public class GameEventDataLogPickupData : GameEventGeneric<Vector3>
+{
+}
