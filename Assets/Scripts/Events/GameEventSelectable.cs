55ec710d2d6ad59224dac1ce55d78b2a9b121689
Roosevelt Boyland III <treyboyland@aol.com>
2025-05-17 12:37:33 -0500

    Create GameEventSelectable.cs

---
+++ Assets/Scripts/Events/GameEventSelectable.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "GameEvent-Selectable-On", menuName = "Events/Data Log Pickup")]
+public class GameEventSelectable: GameEventGeneric<Selectable>
+{
+}
